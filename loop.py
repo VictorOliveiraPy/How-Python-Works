bf24e6fa44a7cf3d4cfd565cedb9378fd67ec772
Victor Hugo Nascimento Oliveira <victor.oliveira@nb-will0492.avista.local>
2021-10-06 13:14:02 -0300

    adding fast loop "c"

--- loop.py
+++ loop.py
@@ -1,5 +1,7 @@
 import timeit
 
+import numpy as numpy
+
 
 def while_loop(n=100_000_000):
     i = 0
@@ -11,7 +13,7 @@ def while_loop(n=100_000_000):
     return s
 
 
-# A parte de interacao do for e escrita em C
+# A parte de interacao do for e escrita em C # verificacoes deixar mais lento o loop
 def for_loop(n=100_000_000):
     s = 0
     for i in range(n):
@@ -23,10 +25,15 @@ def sum_range(n=100_000_000):
     return sum(range(n))
 
 
+def sum_numpy(n=100_000_000):
+    return numpy.sum(numpy.arange(n))
+
+
 def main():
     print('while loop\t\t', timeit.timeit(while_loop, number=1))
     print('for pure\t\t', timeit.timeit(for_loop, number=1))
     print('sum range\t\t', timeit.timeit(sum_range, number=1))
+    print('sum numpy\t\t', timeit.timeit(sum_numpy, number=1))
 
 
 if __name__ == '__main__':
